74563142da08fc4bd5d6368c34aa7440b690b9bc
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-05 20:47:34 +0300

    Расширение Deconstruct для Rectangle

---
+++ HappyNewYearScreenSaver/Extentions/RectangleEx.cs
@@ -0,0 +1,7 @@
+﻿namespace HappyNewYearScreenSaver.Extentions;
+
+public static class RectangleEx
+{
+	public static void Deconstruct(this System.Drawing.Rectangle rect, out int left, out int top, out int wight, out int height)
+		=> (left, top, wight, height) = (rect.Left, rect.Top, rect.Width, rect.Height);
+}
