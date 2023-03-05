a5dccb813aa1e4f818d31ab44df01d7ed150efdf
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-03-05 14:44:38 +0300

    поиск для по календарю

--- HappyNewYearScreenSaver/MainWindow.xaml.cs
+++ HappyNewYearScreenSaver/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 
 namespace HappyNewYearScreenSaver
 {
@@ -7,6 +8,29 @@ namespace HappyNewYearScreenSaver
     /// </summary>
     public partial class MainWindow : Window
     {
-		public MainWindow() => InitializeComponent();
-	}
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        public string Text { get => GetDayName(); }
+
+        public string GetDayName()
+        {
+            var text = File.ReadAllLines(Environment.CurrentDirectory + "/Resourses/ical-favorite-out.ics");
+            var date = DateTime.Now.ToString("yyyyMMdd");
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains("VALUE=DATE:" + date))
+                {
+                    for (int y = i; y > 0; y--)
+                    {
+                        if (text[y].Contains("SUMMARY:"))
+                            return text[y].Replace("SUMMARY:", "");
+                    }
+                }
+            }
+            return "Бывают, деточка, и просто дни";
+        }
+    }
 }
